86c8df2040565c94b313121ceb038f988c85549a
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2022-08-06 15:36:12 +0100

    Update privacy-toggles.js

--- privacy-toggles.js
+++ privacy-toggles.js
@@ -9,7 +9,7 @@ export default {
     },
     analytics: {
       title: 'Analytics',
-      description: `Modrinth uses in-house tools that allow us to get insights into how each user is using the platform
+      description: `Feather uses in-house tools that allow us to get insights into how each user is using the platform
        in order to improve the experience for everyone.\n By enabling this toggle, you allow us to gather information
        across requests. Disabling this toggle will simply remove all Personal Identifying Information from now on.`,
       default: true,
